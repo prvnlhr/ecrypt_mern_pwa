c1d1e58d60404ec9ab8d64eb8a8bee026debb279
Praveen Lohar <praveenlohar.in@gmail.com>
2022-12-04 22:30:28 +0530

    completed : settings component ui

--- client/src/components/dashboardComponent/Dashboard.js
+++ client/src/components/dashboardComponent/Dashboard.js
@@ -16,40 +16,9 @@ const Dashboard = ({ setHeading }) => {
 
   return (
     <div className={styles.dashboardComponent}>
-      DASHBOARD
-      {/* <div className={styles.countWrapper}>
-        <div className={styles.countContainer}>
-          <div className={styles.countDiv}>
-            <p>{cardsArray.length}</p>
-          </div>
-          <div className={`  ${styles.footer} ${styles.footerOne} `}>
-            <div className={styles.underLineDiv1}></div>
-            <p>Cards</p>
-          </div>
-        </div>
-        <div className={styles.countContainer}>
-          <div className={styles.countDiv}>
-            <p>{loginIdsArray.length}</p>
-          </div>
-          <div className={`  ${styles.footer} ${styles.footerOne} `}>
-            <div className={styles.underLineDiv2}></div>
-            <p>Logins</p>
-          </div>
-        </div>
-        <div className={styles.countContainer}>
-          <div className={styles.countDiv}>
-            <p>{docsArray.length}</p>
-          </div>
-          <div className={`  ${styles.footer} ${styles.footerOne} `}>
-            <div className={styles.underLineDiv3}></div>
-            <p>Docs</p>
-          </div>
-        </div>
-      </div>
-      <div className={styles.recentActivityListHeadingDiv}>
-        <p>Recent Activities</p>
-      </div>
-      <ActivityList activities={activitiesArray} /> */}
+      <div className={styles.overviewWrapper}></div>
+      <div className={styles.recentActivitesWrapper}></div>
+      <div className={styles.recentlyAddedWrapper}></div>
     </div>
   );
 };
