0fa85da8e46940886899f8e4a129588ba6c20e10
Praveen Lohar <praveenlohar.in@gmail.com>
2023-02-17 19:57:18 +0530

    update build dir path for production

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use("/", require("./routes/index"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html", "client", "build", "index.html"));
   });
 }
 //SERVER LISTENING
